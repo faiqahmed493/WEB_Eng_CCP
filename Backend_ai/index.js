39577c4672af59dd376fdb41d2aca91499cd22c4
Faiq Ahmed <faiqahmed493@gmail.com>
2026-05-12 12:38:29 +0500

    index.js update

--- Backend_ai/index.js
+++ Backend_ai/index.js
@@ -7,7 +7,11 @@ require('./conn')
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin:"http://localhost:5173"
+    // origin:"http://localhost:5173"
+     origin: [
+        "http://localhost:5173",
+        "https://web-eng-ccp.vercel.app/"
+    ],
     }))
 
 const userRoutes = require('./Routes/user');
